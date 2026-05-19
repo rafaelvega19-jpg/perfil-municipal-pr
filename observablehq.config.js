ea3989234de4e000f3f627453c8073865c0f61ef
rafaelvega19-jpg <rafaelvega19@upr.edu>
2026-05-19 02:42:52 -0400

    remove examples

--- observablehq.config.js
+++ observablehq.config.js
@@ -3,18 +3,9 @@ export default {
   // The app’s title; used in the sidebar and webpage titles.
   title: "Perfil Muncicipal Final",
 
-  // The pages and sections in the sidebar. If you don’t specify this option,
-  // all pages will be listed in alphabetical order. Listing pages explicitly
-  // lets you organize them into sections and have unlisted pages.
-  // pages: [
-  //   {
-  //     name: "Examples",
-  //     pages: [
-  //       {name: "Dashboard", path: "/example-dashboard"},
-  //       {name: "Report", path: "/example-report"}
-  //     ]
-  //   }
-  // ],
+ pages: [
+  { name: "Perfil Municipal", path: "/" }
+],
 
   // Content to add to the head of the page, e.g. for a favicon:
   head: '<link rel="icon" href="observable.png" type="image/png" sizes="32x32">',
